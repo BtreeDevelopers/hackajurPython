c91328e39e7a1470f7afff1075fd259746e4729a
lucas.goncalves <lucas.goncalves@sankhya.com.br>
2023-09-17 14:43:26 -0300

    Gerando o arquivo assinado para o caso de cartão de credito

--- modules/generateDocuments.py
+++ modules/generateDocuments.py
@@ -50,10 +50,10 @@ def generate_propsta_garantia_real(url_list, documento, dadosPagamento, pessoa, 
             story.append(p)
             story.append(Spacer(1, 6))
 
-        # imagem = Image(documento.imagem_io, width=150, height=100)
-        # imagem.hAlign = 'LEFT'
-        #
-        # story.append(imagem)
+        imagem = Image(documento.imagem_io, width=150, height=100)
+        imagem.hAlign = 'LEFT'
+
+        story.append(imagem)
 
         doc.build(story)
 
@@ -182,6 +182,38 @@ def generate_proposta_cartao_credito(url_list, documento, dadosPagamento, pessoa
         # imagem.hAlign = 'LEFT'
         #
         # story.append(imagem)
+
+        doc.build(story)
+
+        url_list.append(save_doc_bucket(pdf_file))
+
+        return url_list
+    except Exception as e:
+        err_msg = f'It was not possible to send the email. Cause: {traceback.format_exc()}'
+        print(err_msg)
+        raise e
+
+def generate_proposta_cartao_credito_assinado(imagem_io, url_list, documento, dadosPagamento, pessoa, numero_cartao_credito, is_pf):
+    try:
+        pdf_file = f"proposta_cartao_credito_{pessoa.nome}_{documento.data_assinatura_contrato}.pdf"
+        pdf_file = pdf_file.replace(" ", "")
+        pdf_file = pdf_file.replace(":", "")
+        doc = SimpleDocTemplate(pdf_file, pagesize=letter)
+        styles = getSampleStyleSheet()
+        story = []
+
+        document = return_propsta_cartao_credito_template(documento, dadosPagamento, pessoa, numero_cartao_credito, is_pf)
+
+        for paragraph in document.split('\n'):
+            p = Paragraph(paragraph.replace('<b>', '<font name="Helvetica-Bold">').replace('</b>', '</font>'),
+                          styles['Normal'])
+            story.append(p)
+            story.append(Spacer(1, 6))
+
+        imagem = Image(imagem_io, width=150, height=100)
+        imagem.hAlign = 'LEFT'
+
+        story.append(imagem)
 
         doc.build(story)
 
@@ -447,3 +479,66 @@ def generate_doc(data):
             url_list = generate_propsta_cartao_debito(url_list, documento, dadosPagamento, pessoaJuridica,
                                                       numero_cartao_debito, is_pf)
             return url_list
+
+def gera_contrato(data):
+    url_list = []
+    tipo = data["tipo"]
+    rua = data["rua"]
+    numero_endereco = data["numero_endereco"]
+    bairro = data["bairro"]
+    cidade = data["cidade"]
+    uf = data["uf"]
+    cep = cep_with_mask(data["cep"])
+    valor = data["valor_divida"]
+    valor_desconto = data["valor_desconto"]
+    vencimento_divida = data["vencimento_divida"]
+    qtd_de_parcela = data["qtd_de_parcela"]
+    is_pf = data["is_pf"]
+    numero_cartao_credito = data["numero_cartao_credito"]
+    url_bucket = data["url_bucket"]
+
+    endereco = return_complete_address(rua, numero_endereco, bairro, cidade, uf, cep)
+
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+    valor_formatado = locale.currency(valor, grouping=True)
+    valor_formatado_por_extenso = numero_por_extenso(valor)
+    valor_descontado = calcula_valor_descontado(valor, valor_desconto)
+    valor_parcela = calculate_valor_parcela(valor_descontado, qtd_de_parcela)
+    valor_parcela = locale.currency(valor_parcela, grouping=True)
+    valor_formatado_descontado = locale.currency(valor_descontado, grouping=True)
+    valor_descontado_por_extenso = numero_por_extenso(valor_descontado)
+
+    data_assinatura_contrato = datetime.now()
+    data_assinatura_formatada = f"{data_assinatura_contrato.day} de {meses[data_assinatura_contrato.month - 1]} de {data_assinatura_contrato.year}"
+
+    imagem_io = get_Image_from_bucket(url_bucket)
+
+    documento = DocumentoPropostaFiador(data_assinatura_formatada, data_assinatura_contrato, imagem_io="null")
+    dadosPagamento = DadosPagamento(valor, valor_formatado, valor_formatado_por_extenso, valor_formatado_descontado,
+                                    valor_descontado_por_extenso, valor_desconto, qtd_de_parcela, valor_parcela)
+    if(tipo == "credito"):
+        if(is_pf):
+            nome = data["nome"]
+            nacionalidade = data["nacionalidade"]
+            estado_civil = data["estado_civil"]
+            cpf = cpf_with_mask(data["cpf"])
+            pessoaFisica = PessoaFisica(nome, nacionalidade, estado_civil, cpf, endereco)
+            url_bucket = generate_proposta_cartao_credito_assinado(imagem_io, url_list, documento, dadosPagamento, pessoaFisica, numero_cartao_credito, is_pf)
+            return url_bucket
+        else:
+            nome_empresa = data["nome"]
+            pj = data["pj"]
+            cnpj = data["cnpj"]
+            cpf_administrador = data["cpf_administrador"]
+            nome_administrador = data["nome_administrador"]
+            nacionalidade_administrador = data["nacionalidade_administrador"]
+            estado_civil_administrador = data["estado_civil_administrador"]
+            pessoaJuridica = PessoaJuridica(nome_empresa, pj, cnpj, endereco, cpf_administrador, nome_administrador,
+                                            nacionalidade_administrador, estado_civil_administrador)
+            url_bucket = generate_proposta_cartao_credito_assinado(imagem_io, url_list, documento, dadosPagamento,
+                                                                   pessoaJuridica, numero_cartao_credito, is_pf)
+            return url_bucket
+
+
+
+
--- services/generateDocumentsService.py
+++ services/generateDocumentsService.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 import traceback
-from modules.generateDocuments import generate_doc
+from modules.generateDocuments import generate_doc, gera_contrato
 
 router = Blueprint('document', __name__, url_prefix='/document')
 
@@ -8,11 +8,11 @@ router = Blueprint('document', __name__, url_prefix='/document')
 def teste():
     return {"message": "hello world!!"}
 
-@router.route("/gerar-propostas", methods=["POST"])
-def change_password_router():
+@router.route("/gerar-contrato-assinado", methods=["POST"])
+def gera_contrato_assinado():
     try:
         data = request.json
-        bucket_link = generate_doc(data)
+        bucket_link = gera_contrato(data)
         return {'error': 'false', 'message': 'Arquivo salvo com sucesso', "url": bucket_link, 'status': 200}
     except Exception as e:
         err_msg = f'It was not possible to send the email. Cause: {traceback.format_exc()}'
